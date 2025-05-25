4a82dfb85fac4a8d3116bafe81c74123bb4635b6
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2025-05-25 14:33:04 +0500

    Create adfeed component

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import FormPage from '@/pages/FormPage';
 import DefaultPage from '../pages/DefaultPage';
-import AdFeedPage from '@/pages/AdFeedPage.tsx';
+import { AdFeedPage } from '@/pages/AdFeedPage.tsx';
 
 export const privateRoutes = [
   { path: '/defaultpage', component: DefaultPage },
