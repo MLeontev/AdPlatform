fe83adb74b14b421a488083fe505755520efa97d
LMax <max.leontiev@mail.ru>
2025-06-17 15:47:41 +0500

    Add Header

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,19 +1,18 @@
 import { AdFeedPage } from '@/pages/AdFeedPage.tsx';
 import { AdPage } from '@/pages/AdPage.tsx';
-import DefaultPage from '@/pages/DefaultPage';
 import { FormPage } from '@/pages/FormPage.tsx';
 import { LoginPage } from '@/pages/LoginPage.tsx';
 import { RegisterPage } from '@/pages/RegisterPage.tsx';
 
 export const privateRoutes = [
-  { path: '/defaultpage', component: DefaultPage },
+  // { path: '/defaultpage', component: DefaultPage },
   { path: '/adform', component: FormPage },
   { path: '/adfeed', component: AdFeedPage },
   { path: '/ad', component: AdPage },
 ];
 
 export const publicRoutes = [
-  { path: '/defaultpage', component: DefaultPage },
+  // { path: '/defaultpage', component: DefaultPage },
   { path: '/adfeed', component: AdFeedPage },
   { path: '/ad', component: AdPage },
   { path: '/login', component: LoginPage },
